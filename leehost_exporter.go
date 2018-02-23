c78cc5491c1d89a99e7267d111e0470c68c9e042
leeningli <1507045477@qq.com>
2018-02-23 13:17:21 +0800

    add iostat monitor
    add iostat monitor

--- leehost_exporter.go
+++ leehost_exporter.go
@@ -75,32 +75,48 @@ func ExporterHandler(w http.ResponseWriter, r *http.Request){
 	host := get_hostip()
 	var metrics_url string = ""
 	io.WriteString(w, "#this is monitor for host, dev by lee\n")
-	uptime_cmd := `uptime|awk '{print $11}'|awk -F"," '{print $1}'`
-	metrics_url = fmt.Sprintf("uptime{host=\"%s\"} %s\n", host, exec_shell(uptime_cmd))
-	io.WriteString(w, metrics_url)
-	
-	
-	logstash_cmd := `ps -ef|grep -v grep|grep logstash|awk '{print $2}'|wc -l`
-	metrics_url = fmt.Sprintf("logstashIsExist{host=\"%s\"} %s\n", host, exec_shell(logstash_cmd))
+    	uptime_cmd := `uptime|awk '{print $11}'|awk -F"," '{print $1}'`
+    	metrics_url = fmt.Sprintf("linux_uptime_load{ip=\"%s\",time=\"5min\"} %s\n", host, exec_shell(uptime_cmd))
+    	io.WriteString(w, metrics_url)
+
+    	logstash_cmd := `ps -ef|grep -v grep|grep logstash|awk '{print $2}'|wc -l`
+    	metrics_url = fmt.Sprintf("logstashIsExist{host=\"%s\"} %s\n", host, exec_shell(logstash_cmd))
+   	io.WriteString(w, metrics_url)
+
+    	flume_cmd := `ps -ef|grep -v grep|grep flume|awk '{print $2}'|wc -l`
+    	metrics_url = fmt.Sprintf("flumeIsExist{host=\"%s\"} %s\n", host, exec_shell(flume_cmd))
+    	io.WriteString(w, metrics_url)
+
+    	zabbix_cmd := `ps -ef|grep -v grep|grep zabbix|awk '{print $2}'|wc -l`
+    	metrics_url = fmt.Sprintf("zabbixIsExist{host=\"%s\"} %s\n", host, exec_shell(zabbix_cmd))
+    	io.WriteString(w, metrics_url)
+
+    	io.WriteString(w, "#this is monitor for salt process is exist\n")
+    	salt_cmd := `ps -ef|grep -v grep|grep salt|awk '{print $2}'|wc -l`
+    	metrics_url = fmt.Sprintf("saltIsExist{host=\"%s\"} %s\n", host, exec_shell(salt_cmd))
+    	io.WriteString(w, metrics_url)
+
+    	io.WriteString(w, "#this is monitor for openfiles \n")
+    	openfile_cmd := `lsof |awk '{print $3}' |grep -v "TID" |sort |uniq -c|awk '{print "user_open_files{host=\"` + host +`\",user=\""$2"\"}\t"$1}'`
+    	metrics_url = fmt.Sprintf("%s", exec_shell(openfile_cmd))
+    	io.WriteString(w, metrics_url)
+
+	/*
+	io.WriteString(w, "#this is monitor for frontend-app connect to redis,only list connect_cnt>10;if your host is redis host,forget it.\n")
+	frontend_redis_cmd := `netstat -pan|grep 6379|awk '{print $7}'|awk -F"/" '{print $1}'|sort |uniq -c|awk '{print "pid_redis_cnt{host=\"` +host +`\",pid=\""$2"\"}\t"$1}'`
+	//frontend_redis_cmd := `netstat -pan|grep 6379|awk '{print $7}'|awk -F"/" '{print $1}'|sort |uniq -c|awk '{print "pid_redis_cnt{pid=\""$2"\"}\t"$1}'`
+	metrics_url = fmt.Sprintf("%s", exec_shell(frontend_redis_cmd))
 	io.WriteString(w, metrics_url)
 
-	flume_cmd := `ps -ef|grep -v grep|grep flume|awk '{print $2}'|wc -l`
-	metrics_url = fmt.Sprintf("flumeIsExist{host=\"%s\"} %s\n", host, exec_shell(flume_cmd))
-	io.WriteString(w, metrics_url)
+	io.WriteString(w, "#this is monitor for redis connect status on redis host,only list connect_cnt>10;if your host is not redis host,forget it.\n")
+	redis_cmd := `netstat -an|grep 6379|awk '{print $5}' |awk -F":" '{print $1}'|grep -v "0.0.0.0"|sort|uniq -c |awk '{if($1>10) print "redis_connect_cnt{remote_host=\""$2"\"}\t"$1}'`
+	metrics_url = fmt.Sprintf("%s", exec_shell(redis_cmd))
+	io.WriteString(w, metrics_url)*/
 
-	zabbix_cmd := `ps -ef|grep -v grep|grep zabbix|awk '{print $2}'|wc -l`
-	metrics_url = fmt.Sprintf("zabbixIsExist{host=\"%s\"} %s\n", host, exec_shell(zabbix_cmd))
-	io.WriteString(w, metrics_url)
-
-	io.WriteString(w, "#this is monitor for salt process is exist\n")
-	salt_cmd := `ps -ef|grep -v grep|grep salt|awk '{print $2}'|wc -l`
-	metrics_url = fmt.Sprintf("saltIsExist{host=\"%s\"} %s\n", host, exec_shell(salt_cmd))
-	io.WriteString(w, metrics_url)
-	
-	io.WriteString(w, "#this is monitor for openfiles \n")
-	current_user := exec_shell("whoami")
-	openfile_cmd := `lsof |awk -v user=${current_user} '{if($3==user) print $0}'|wc -l`
-	metrics_url = fmt.Sprintf("openfiles{host=\"%s\",user=\"%s\"} %s\n", host, current_user, exec_shell(openfile_cmd))
+	io.WriteString(w, "#this is monitor for disk io on host.only tps>10\n")
+	diskio_cmd := `iostat -d -k|awk '{if($2>10) print $0}'|grep -v "Device"|grep -v "Linux"|grep -v "sdb"|awk '{printf "disk_io_tps{disk=\"%s\",host=\"%s\"}\t%f\n",$1,"`+host+`",$2}'`
+	//fmt.Println(diskio_cmd)
+	metrics_url = fmt.Sprintf("%s", exec_shell(diskio_cmd))
 	io.WriteString(w, metrics_url)
 }
 
